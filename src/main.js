39b1ef73df2e6cea5a7de4edd3ebd9479f0048a2
orangeman7201 <84920430+orangeman7201@users.noreply.github.com>
2021-07-18 09:23:29 +0900

    Revert "Vue課題②-1"

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import InputFormat from './components/InputFormat.vue';
-import Questions from './components/Questions.vue';
-import AllData from './components/AllData.vue';
-import FreeFormat from './components/FreeFormat.vue';
-import router from './router';
-import store from './store';
 
-
-
-Vue.config.productionTip = true
-Vue.component('InputFormat', InputFormat);
-Vue.component('Questions', Questions );
-Vue.component('FreeFormat', FreeFormat);
-Vue.component('AllData', AllData);
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
   render: h => h(App),
 }).$mount('#app')
